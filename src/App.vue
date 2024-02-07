8c9d484571f5f35d9af3e4e7259b1f5e66db7318
D-Raff <damonraffels@gmail.com>
2024-02-07 14:48:51 +0200

    added nav skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
+  <navbar/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import navbar from './components/navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    navbar,
+
+  }
+
+}
+
+</script>
 
 <style>
 #app {
